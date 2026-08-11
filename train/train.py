9e662338ef7ff0f3771d109d1e08398a27442fee
Victor Agu <victoragu14x@gmail.com>
2026-08-11 04:58:59 +0100

    I compromised

--- train/train.py
+++ train/train.py
@@ -3,27 +3,46 @@ Implementation of siamese network
 using keras io as guide 
 
 Goals :
-
-1. Light weight but efficeint model -> Separable conv2d 
+1. Light weight but efficient model -> Separable conv2d 
 2. Efficient Model 
-
 """
 
 import tensorflow as tf 
 import keras 
-from keras import layers , Model 
-from utils import load_datasets , make_pairs , pairing 
+from keras import layers 
+from utils import get_filepaths_and_labels, make_pair_indices, build_pair_dataset
+
+
+# =========================================DATA LOADING AND HYPER PARAM============================================ #
 
 path = r'C:\Users\blade_mx4\Documents\Datasets\DOG_CAT'
 
-X , Y = load_datasets(path)
 
-#print(X.shape) 
+filepaths, labels, class_names = get_filepaths_and_labels(path)
+pair_indices, pair_labels = make_pair_indices(labels)
 
-#X_train , Y_train = make_pairs(X,Y) 
-
-train_ds = make_pairs(X,Y) 
+filepaths_tensor = tf.constant(filepaths)  # needed for tf.gather inside the tf.data graph
+train = build_pair_dataset(filepaths_tensor, pair_indices, pair_labels, batch_size=8)
 
 # ======================================= MODEL ================================== #
 
-def 
+def Model() : 
+    Input = layers.Input((224,224))  
+    x = layers.Rescaling(scale=1./127.5, offset=-1)(Input)
+
+    x = layers.Conv2D(16, kernel_size=(3,3), strides=(1,1), activation='tanh')(x) 
+    x = layers.Conv2D(32, kernel_size=(3,3), strides=(1,1), activation='tanh')(x) 
+    x = layers.BatchNormalization()(x)
+
+    x = layers.SeparableConv2D(64, kernel_size=(3,3), strides=(1,1), activation='tanh')(x) 
+    x = layers.SeparableConv2D(128, kernel_size=(3,3), strides=(1,1), activation='tanh')(x) 
+
+    x = layers.AveragePooling2D(pool_size=(2,2))(x)
+    x = layers.Flatten()(x)
+
+    x = layers.BatchNormalization()(x) 
+
+    x = layers.Dense(64, activation='tanh')(x)  # final embedding layer, 64D
+    return keras.Model(Input, x) 
+
+Model().summary()
