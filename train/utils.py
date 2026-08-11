3e870ace05b716fda5dbcf6ce227241f296934bb
Victor Agu <victoragu14x@gmail.com>
2026-08-11 03:15:25 +0100

    Rewriting the utils.py from scratch

--- train/utils.py
+++ train/utils.py
@@ -1,19 +1,15 @@
 # functions for the model training 
 import tensorflow as tf 
-
 import keras 
 from keras import layers , Model 
-from keras.utils import image_dataset_from_directory ,load_img , img_to_array
 import numpy as np 
 import random
+from keras.utils import image_dataset_from_directory 
+SHAPE = (224,224) 
 
-
-
-SHAPE = (224,224)
-
-def load_imgs(path) : 
-    ds = image_dataset_from_directory (
-        path , image_size=(224,224) , batch_size = 16 ,
-        label_mode='int' 
+def load_datasets(path) : 
+    image_dataset_from_directory(
+        path , image_size = (244,244) ,
+        shuffle =True , label_mode = 'int' 
     )
-    return ds 
+    
