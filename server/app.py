3b865f69f39c660784befc9595b1ef24f37db367
Victor Agu <victoragu14x@gmail.com>
2026-08-15 06:01:37 +0100

    Minor Changes Couldnt Implement Async

--- server/app.py
+++ server/app.py
@@ -8,4 +8,6 @@ def Server_Main() :
     app.register_blueprint(img_server_bp) 
     return app 
 
-if __name__ == "__main__" : Server_Main().run(port=9000,debug=True) #<-- Naughty oneliner
+if __name__ == "__main__" : 
+    
+    Server_Main().run(port=9000,debug=True) #<-- Naughty oneliner
