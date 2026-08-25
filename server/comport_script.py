d7dec6e7f2cfb338df9deb934f7d0d1beedd8b8a
Victor Agu <victoragu14x@gmail.com>
2026-08-25 21:34:54 +0100

    Didnt commit yesterday

--- server/comport_script.py
+++ server/comport_script.py
@@ -2,6 +2,10 @@ import serial
 import serial.tools.list_ports 
 
 
-
-def port_look() : 
-    port_list = []
+def port_look() :
+    port_list = []  
+    for port in serial.tools.list_ports.comports() : 
+        available = port.device 
+        port_list.append(available)
+        if available == None : break 
+    return port_list #print(port_list)
