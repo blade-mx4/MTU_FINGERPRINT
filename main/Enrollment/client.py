5132913629e3bd5c7ce94871f480da517905182d
Victor Agu <victoragu14x@gmail.com>
2026-08-31 15:32:13 +0100

    re-writing in classes

--- main/Enrollment/client.py
+++ main/Enrollment/client.py
@@ -21,83 +21,99 @@ import requests as rq
 import os 
 import json 
 import pandas as pd 
+from threading import Thread 
+import logging as log
 
 # ====================== CONFIGS and HYPER PARAM ============================== #
 
 url =   'http://127.0.0.1:9000/image_server/upload'                             #<-- Route to upload images 
-
-
 db_dir  = 'DB' #<-- folder name 
 cwd_dir = os.getcwd() 
-
 cw_fldr =  os.path.join(cwd_dir , db_dir)
-
 os.makedirs(db_dir,exist_ok=True)
 
+log_folder_mk = os.makedirs('Enrollment_LOG',exist_ok=True)    #<--- Folder for log
+log_file = os.path.join(cw_fldr,'Enrollment_LOG')
 
-def CSV_DB(id:int ,name : str , surname : str , matric : int ,dept : str , level : int ) ->bool :
-    """
-    Function for creating main csv 
+log_file = f"{log_file}/Enrollment.log"
+
+
+# ======================================= #
+
+class Client : 
+    def __init__(self,port,baud_rate,file_name,ID : int , Name:str , Surname:str , Matric : int ,Dept : str,  Level :int ):
+        self.port = port 
+        self.baud_rate = baud_rate 
+        self.filename = file_name 
+        self.ID = ID 
+        self.Name = Name 
+        self.Surname = Surname 
+        self.Matric = Matric 
+        self.Dept = Dept 
+        self.Level = Level        
+
+    def main_csv(self) : 
+        try :
+            level = [100,200,300,400,500]
+            self.Matric = str(self.Matric)
+
+            if self.Level not in level :
+                return print("INVALID LEVEL")
+            else : pass 
+
+            if len(self.Matric) == 11 : pass 
+            else : return print("INVALID Matric")
+
+            if self.Name and self.Surname == None : return print(" Input Complete Name ")
+            else : pass 
+
+
+            data = {
+                "ID"      : [self.ID], 
+                "Name"    : [self.Name] ,
+                "Surname" : [self.Surname],
+                "Matric"  : [self.Matric],
+                "Level"   : [self.Level],
+                "Dept"    : [self.Dept],
+
+            }
+
+            Data = pd.DataFrame(data)
+            path = f"{cw_fldr}/DB.csv"
+            Data.to_csv(path,index=False)
+            print(f"CSV CREATED -> {path}")
+            return True 
+        except Exception as e : print(f"ERROR [ {e} ]")
+
+    def post_data_img(self) :
+        self.filename = f"{self.filename}.bmp"
     
-    """ 
-    try :   #<-- Error Handling for inputs  
-        
-        # ====================== Conditionals =============================== # 
-        Level = [100,200,300,400,500]        #<-- Level Error Checker
-        matric = str(matric)                 #<-- Had to converit to string to be able to check the length 
-
-        if level not in  Level : 
-            return print("INVALID Level " )  #<--- Acompanied by a Error Message Box
-        else : pass 
-
-        if len(matric) == 11 :   #<-- Matric Error checker    
-            pass 
-        else : return print("Invalid Matric ")  #<-- intended 1 line but compromise
-
-        if name and surname == None : return print("NO Name Provided") 
-        else : pass 
-
-
         data = {
-            "ID"     :[id],
-            "Name"   :[name] ,
-            "Surname":[surname] ,
-            "Matric" :[matric],
-            "Dept"   :[dept] ,
-            "Level"  :[level] 
+            "ID"      : [self.ID], 
+            "Name"    : [self.Name] ,
+            "Surname" : [self.Surname],
+            "Matric"  : [self.Matric],
+            "Level"   : [self.Level],
+            "Dept"    : [self.Dept],
         }
+        try : 
+            if getFingerprintImage(portNum=self.port,baudRate=self.baud_rate,outputFileName=self.filename) == True :
+                student_data = {
+                    "student_data" : json.dumps(data)
+                }
+                student_img = {
+                    "student_img"  : open(self.filename, 'rb')
+                }
+                post = rq.post(
+                    url = url ,
+                    data=student_data ,
+                    files=student_img
+                )
+                if self.main_csv() == True : 
+                    return print(post.json()) , True
 
-        Data = pd.DataFrame(data)
-        path = f"{cw_fldr}/DB_GLOBAL.csv" 
-        Data.to_csv(path,index=False,header=False,mode='a')  #<-- SO since am using csv -> sql i can use this to append to existing files
-        print("CSV Created")
+        except Exception as e :
+            print(f"ERROR =>[ {e} ]")
 
-    except Exception as e : print(f"ERROR{e}") 
-
-
-
-def post_data_img(port,baud_rate,file_name,ID : int , Name:str , Surname:str , Matric : int ,Dept : str,  Level :int ) : 
-    file_name = f"{file_name}.bmp"
-    data = {
-        "ID"      : ID ,
-        "Name"    : Name ,
-        "Surname" : Surname ,
-        "Matric"  : Matric ,
-        "Level"   : Level, 
-        "Dept"    : Dept
-    }
-    try : 
-        if getFingerprintImage(port,baud_rate,file_name) == True : 
-            student_data = {"student_data" : json.dumps(data)}
-            student_img = {"student_img" : open(file_name ,'rb')} 
-            feed = rq.post(
-                url ,
-                data=student_data ,
-                files=student_img 
-            ) 
-            if CSV_DB(ID,Name,Surname,Matric,Dept,Level) == True :
-                return print(feed.json()) , True
-        
-    except Exception as e : print(f"ERROR[{e}]") 
-if __name__ == "__main__" : post_data_img('COM9' , 115200 ,'diasasmond',212,'Diond','vuc',24010305032,'SWE',200)
-
+client = Client('COM9' , 115200 ,'diasasmond',212,'Diond','vuc',24010305032,'SWE',200)
+client.post_data_img()
