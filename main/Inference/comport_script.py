6112695045b6b9261b084a37bf647f8e1d543cf2
Victor Agu <victoragu14x@gmail.com>
2026-09-01 09:41:39 +0100

    fixed the multiport connection issue

--- main/Inference/comport_script.py
+++ main/Inference/comport_script.py
@@ -1,75 +1,45 @@
-"""
-This Script runs through a singular pizero conected to let say 4 esp32
-
-            [    esp32 --> pizero/pi --wifi--> Server  ]
-
-then loops through automatically throughthe available ports and connectes automatically
-instead of hardcoding ports{which works}
-also allowing multiport connection from different ports with threading 
-
-
-todo : 
-
-"""
-#May turn this into a module 
 from serial import Serial 
-import serial.tools.list_ports 
-from threading import Thread
-from time import sleep
-from Client import getImage
-
-BAUDRATE = 115200
-def port_look() -> list :   #<-- Search for port available and returns list of port   
-    global port             #<-- Since am using the PORTNO as an IP Address like system to help the pi route back data to the port no
-    port_list = []  
-    for port in serial.tools.list_ports.comports() : 
-        available = port.device 
-        port_list.append(available)
-        if available == None : break 
-    return port_list              #<--- returns the comport list eg { comport 10,comport 11 } #print(port_list)
+from serial.tools import list_ports 
+from time import sleep 
+from threading import Thread 
 
 
-def ser_init(port_name : str ) -> bool  : #<--- serial port connection init 
-    global ser #<--  So other functions can acces it 
-    while True :
-        try :
-            # for i in range(len(port_look())) :  #<-- To just keep an open range of ports then close when range gaped
-            ser = Serial(port=port_name, baudrate=BAUDRATE,timeout=None) 
+
+# ==== Configs ==== #
+BAUDRATE = 115200 
+
+
+
+def port_find() ->list[str]:
+    port_list  = [] 
+    for port in list_ports.comports(): 
+        ports = port.device
+        port_list.append(ports)
+    print(port_list)
+    return port_list 
+
+
+def ser_init(port : str ):
+        try : 
+            ser = Serial(port=port,baudrate=BAUDRATE , timeout=None)
+
             if ser.is_open : 
-                print(f"Connected -> [{port_name}]")
-                return True
-                
+                print(port)
+                while True : 
+                    ser.read()
         except Exception as e : 
-            print(f"ERROR -> [ {e} ] ")         #<-- Essential for debugging pyserial error r suprizingliy detailed 
-            sleep(1)                             #<-- Anti os Spamming
+            print(f"Erorr -> {e}")
 
-def multi_port() :                                 #<-- threading implementation
-    port_list = port_look() 
-    threads = []                                  #<-- thread pool , felt like calling it that nothing u can do about it 
 
-    for port in port_list : 
-        t = Thread(target=ser_init, args=(port,)) #<--  heads up due to iteration port is a str
-        threads.append(t)
-        t.start()
-    for t in threads : t.join()                     #<--  race condition prevention 
+def thread_er () : 
+    ports_list = port_find () 
+    thread = []
 
-def main() : 
-    multi_port() #<-- initalize multithrading 
-    if ser_init() == True : 
-        if ser.is_open : 
-            line = ser.read_until(b' Input ID : ').decode('utf-8')
-            print(line)
+    for port in ports_list : 
+        t = Thread(target=ser_init , args=(port,))
+        thread.append(t)
+        t.start() 
+    for t in thread :
+        t.join()
 
-            sleep(1) 
-
-            user_input = input("Enter ID : ").strip()
-            ser.write((user_input + '\n').encode()) 
-
-            sleep(0.1)
-
-            response = ser.read(ser.in_waiting or 1 ).decode(errors='replace') #<--  to read the remaining bytes 
-            print(response) 
-            
-
-if __name__ == "__main__" : 
-    
+thread_er()
